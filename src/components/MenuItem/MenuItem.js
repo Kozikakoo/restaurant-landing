70359bf7a590deed507aba4d0bceb2ca42980d72
Anastasiya Nagornaya <kozikakoo@yandex.ru>
2023-01-23 14:36:27 +0300

    feat: finished Menu component;

---
+++ src/components/MenuItem/MenuItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MenuItem() {
+  return(
+      <li className="menu__item">
+          <div className="menu__item-block">
+              <h4 className="menu__dish">Pizza quatro stagioni &#46;&#46;&#46;&#46;</h4>
+              <p className="menu__dish-desc">Integer ullamcorper neque eu purus euismod</p>
+          </div>
+          <p className="menu__dish-price">55,68 USD</p>
+      </li>
+  )
+}
+
+export default MenuItem;
