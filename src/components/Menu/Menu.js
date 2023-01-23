d9ce6d7ad3c88dac0f878c4b1d2ef1df3b30c869
Anastasiya Nagornaya <kozikakoo@yandex.ru>
2023-01-23 14:36:13 +0300

    feat: finished Menu component;

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "../Title/Title";
+import MenuItem from "../MenuItem/MenuItem";
 
 function Menu() {
     return (
@@ -9,25 +10,18 @@ function Menu() {
                 molestie est a, tempor
                 magna.</p>
             <ul className="menu__categories">
-                <li className="menu__category"><a href="#">SOUPE</a></li>
-                <li className="menu__category"><a href="#">PIZZA</a></li>
-                <li className="menu__category"><a href="#">PASTA</a></li>
-                <li className="menu__category"><a href="#">DESERT</a></li>
-                <li className="menu__category"><a href="#">WINE</a></li>
-                <li className="menu__category"><a href="#">BEER</a></li>
-                <li className="menu__category"><a href="#">DRiNKS</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">SOUPE</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">PIZZA</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">PASTA</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">DESERT</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">WINE</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">BEER</a></li>
+                <li className="menu__category"><a className="menu__link" href="#">DRiNKS</a></li>
             </ul>
             <ul className="menu__items">
-                <li className="menu__item">
-                    {
-
-                    }
-                    <div>
-                        <h4>Pizza quatro stagioni &#46;&#46;&#46;&#46;</h4>
-                        <p>Integer ullamcorper neque eu purus euismod</p>
-                    </div>
-                    <p>55,68 USD</p>
-                </li>
+                {
+                    [...Array(21)].map((item, index) => <MenuItem key={index}/>)
+                }
 
             </ul>
         </section>
